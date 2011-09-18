a14292c5f45b6b182a719d07c88b40319f208ba3
miles <miles@thespecials.com>
2011-09-18 21:43:34 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tokyorails::Application.config.secret_token = '7c68a46d64a8b940d8a466d4653481d2d6a7164698f9044f727902fbf207117d8f7c85d97d10a835403f672af2c9b987ad7f977d6c31ff58f91a88c99092a291'
