ec6b11c20778b5ddab69bb4f110e4b30b8647753
miles <miles@thespecials.com>
2011-09-24 08:14:39 +0900

    remove test unit

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,9 @@ module Tokyorails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
