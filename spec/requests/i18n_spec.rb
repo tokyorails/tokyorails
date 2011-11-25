c93a2631bdfc99d761176906be01d5c150cac212
miles <miles@thespecials.com>
2011-11-26 00:22:15 +0900

    refactor locale tabs to use css for selected state

--- spec/requests/i18n_spec.rb
+++ spec/requests/i18n_spec.rb
@@ -7,21 +7,9 @@ feature "Internationalization" do
     page.should have_content('The TOKYO Rails Meetup Group is for web engineers')
   end
 
-  scenario "viewing the default url should have the 'english' tab selected" do
-    visit root_url
-    page.should_not have_css('li.active', :text => '日本語')
-    page.should have_css('li.active', :text => 'English')
-  end
-
   scenario "clicking the '日本語' tab should show the page in Japanese" do
     visit root_url
     click_link('日本語')
     page.should have_content('TOKYO Rails Meetupはルビーオンレイルズフレームワーク')
-  end
-
-  scenario "viewing a japanese path should have the '日本語' tab selected" do
-    visit '/ja'
-    page.should have_css('li.active', :text => '日本語')
-    page.should_not have_css('li.active', :text => 'English')
   end
 end
