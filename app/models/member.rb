ef7732a7e2a56c576316897e3e22c6a71ec87dc3
miles <miles@thespecials.com>
2011-12-05 02:15:59 +0900

    temp fix for heroku PG issues

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,8 +5,9 @@ class Member < ActiveRecord::Base
   validates_uniqueness_of :github_username, :allow_blank => true
 
   has_one :image
-  
+
   def photo
-    self.image || Image.create(:member_id => id, :file_url => photo_url) unless photo_url.blank?
+#    self.image || Image.create(:member_id => id, :file_url => photo_url) unless photo_url.blank?
+    Image.where(:member_id => id).first || Image.create(:member_id => id, :file_url => photo_url) unless photo_url.blank?
   end
 end
