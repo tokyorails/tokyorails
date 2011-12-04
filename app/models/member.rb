eff40755040d193acec5912a761ff843dc3cd973
miles <miles@thespecials.com>
2011-12-05 02:21:39 +0900

    temp fix for heroku PG issues

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,7 +7,6 @@ class Member < ActiveRecord::Base
   has_one :image
 
   def photo
-#    self.image || Image.create(:member_id => id, :file_url => photo_url) unless photo_url.blank?
-    Image.where(:member_id => id).first || Image.create(:member_id => id, :file_url => photo_url) unless photo_url.blank?
+    self.image || Image.create(:member_id => id.to_s, :file_url => photo_url) unless photo_url.blank?
   end
 end
