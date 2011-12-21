e599a44e275a9c82b0b23fc93dc964484ca2fe96
miles <miles@thespecials.com>
2011-12-21 19:38:34 +0900

    remove utf8 hackery

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def sanitize(untrusted_string)
-    cleaned = ""
-    untrusted_string.force_encoding Encoding::BINARY
-    untrusted_string.codepoints.each do |c|
-      cleaned += c.chr('UTF-8')
-    end
-    cleaned
-  end
 end
