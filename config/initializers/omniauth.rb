270e7694053638c3316e94efb9db5cf712f4c143
miles <miles@thespecials.com>
2011-12-24 18:20:09 +0900

    use env vars for meetup key and secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :meetup, 'dge0tlogc6cf2rfb2b0pppovg3', 'jkh9u2gk5krj4ki42f5v75cel8'
+  provider :meetup, ENV['MEETUP_KEY'], ENV['MEETUP_SECRET']
 end
