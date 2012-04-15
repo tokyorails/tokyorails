0a7ccc2e3b43da91c2b1f207d335af7d7698308a
miles <miles@thespecials.com>
2012-04-15 21:11:13 +0900

    add smtp conf

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,1 +1,12 @@
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'gmail.com',
+  :user_name            => ENV['SMTP_USERNAME'],
+  :password             => ENV['SMTP_PASSWORD'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true
+}
+
 ActionMailer::Base.default_url_options[:host] = Rails.application.config.default_url
