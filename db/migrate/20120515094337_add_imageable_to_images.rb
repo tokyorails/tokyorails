35466693b14bb79d4908a77209ca70dd2a8711be
miles <miles@thespecials.com>
2012-05-18 10:02:30 +0900

    make migration postrgress friendly

--- db/migrate/20120515094337_add_imageable_to_images.rb
+++ db/migrate/20120515094337_add_imageable_to_images.rb
@@ -2,13 +2,13 @@ class AddImageableToImages < ActiveRecord::Migration
   def self.up
     change_table :images do |t|
       t.references :imageable, :polymorphic => true
-      execute "UPDATE `images` SET imageable_id = member_id WHERE imageable_id IS NULL"
-      execute "UPDATE `images` SET imageable_type = 'Member' WHERE imageable_type IS NULL"
+      execute "UPDATE images SET imageable_id = member_id WHERE imageable_id IS NULL"
+      execute "UPDATE images SET imageable_type = 'Member' WHERE imageable_type IS NULL"
       t.remove_index :member_id
       t.remove :member_id
     end
   end
-  
+
   def self.down
     change_table :images do |t|
       t.remove_references :imageable, :polymorphic => true
