2c3025b47da01013ca0466791b696d9ad6445561
miles <miles@thespecials.com>
2012-07-12 22:45:56 +0900

    fix specs

--- spec/requests/projects_index_spec.rb
+++ spec/requests/projects_index_spec.rb
@@ -6,7 +6,7 @@ feature "Projects" do
     project = Factory(:project)
 
     visit projects_path
-    page.should have_content('Member Projects')
+    page.should have_content('Projects')
     page.should have_content('Cool Project')
   end
 
