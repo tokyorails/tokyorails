fcc18cf7b0ce648cfa1e3b9adce4209c3914f538
miles <miles@thespecials.com>
2012-08-02 18:45:17 +0900

    adds precompilation for print css

--- config/application.rb
+++ config/application.rb
@@ -45,7 +45,7 @@ module Tokyorails
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.precompile += %w[active_admin.css active_admin.js]
+    config.assets.precompile += %w[active_admin.css active_admin.js print.css]
 
     config.assets.initialize_on_precompile = false
 
